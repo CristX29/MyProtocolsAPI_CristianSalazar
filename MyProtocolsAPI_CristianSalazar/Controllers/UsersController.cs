a9b93d194cf5a93878041dda4ca39270e4e52318
Admin <Admin@DESKTOP-A0CTDNO>
2023-07-26 22:01:06 -0600

    semana 11

--- MyProtocolsAPI_CristianSalazar/Controllers/UsersController.cs
+++ MyProtocolsAPI_CristianSalazar/Controllers/UsersController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MyProtocolsAPI_CristianSalazar.Attributes;
 using MyProtocolsAPI_CristianSalazar.Models;
+using MyProtocolsAPI_CristianSalazar.ModelsDTOs;
 
 namespace MyProtocolsAPI_CristianSalazar.Controllers
 {
@@ -72,6 +73,70 @@ namespace MyProtocolsAPI_CristianSalazar.Controllers
             }
 
             return user;
+        }
+
+        [HttpGet("GetUserInfoByEmail")]
+        public ActionResult<IEnumerable<UserDTO>> GetUserInfoByEmail(string Pemail)
+        {
+            //acá creamos un linq que combina informacion de dops entidades
+            //(user inner userrole) y la agrega en el objeto DTO de Usuario
+
+            var query = (from u in _context.Users
+                         join ur in _context.UserRoles on
+                         u.UserRoleId equals ur.UserRoleId
+                         where u.Email == Pemail && u.Active == true
+                         && u.IsBlocked == false
+                         select new
+                         {
+                             idusuario = u.UserId,
+                             correo = u.Email,
+                             contrasennia = u.Password,
+                             nombre = u.Name,
+                             correorespaldo = u.BackUpEmail,
+                             telefono = u.PhoneNumber,
+                             direccion = u.Address,
+                             activo = u.Active,
+                             establoqueado = u.IsBlocked,
+                             idrol = ur.UserRoleId,
+                             descripcionrol = ur.Description
+
+                         }).ToList();
+
+            //creamos un objeto del tipo que retorna la funcion
+
+
+            List<UserDTO> list = new List<UserDTO>();
+            foreach (var item in query) 
+            {
+                UserDTO NewItem = new UserDTO()
+                {
+                    IDUsuario = item.idusuario,
+                    Correo = item.correo,
+                    Contrasennia = item.contrasennia,
+                    Nombre = item.nombre,
+                    CorreoRespaldo = item.correorespaldo,
+                    Telefono = item.telefono,
+                    Direccion = item.direccion,
+                    Activo = item.activo,
+                    EstaBloqueado = item.establoqueado,
+                    IDRol = item.idrol,
+                    DescripcionRol = item.descripcionrol,
+
+
+
+                };
+
+                list.Add(NewItem);
+
+            }
+
+            if (list == null)
+            {
+                return NotFound();
+            }
+            return list;
+
+
         }
 
         // PUT: api/Users/5
